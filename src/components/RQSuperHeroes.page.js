fd4547d70ac724c352241dc92c35307fb86fc7a7
abubakkariiui <abubakkarmit@gmail.com>
2023-02-21 11:14:31 +0500

    explore more details about react query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 import { useQuery } from "react-query";
 
@@ -5,10 +6,30 @@ const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
+const onSuccess = (data) => {
+  console.log("Perform side effect after data fetching.", data);
+};
+
+const onError = (error) => {
+  console.log("Perform side effect after encoutering error.", error);
+};
+
 export const RQSuperHeroesPage = () => {
   const { data, isLoading, isError, error, isFetching } = useQuery(
     "super-heros",
     fetchSuperHeroes,
+    {
+      onSuccess,
+      onError,
+      select: (data) => {
+        const superHeroeNames = data.data.map((hero) => hero.name);
+        return superHeroeNames;
+      },
+    }
+    // {
+    //   refetchInterval: 2000                  // refetch at specific interval
+    //   refetchIntervalInBackground: true
+    // }
     // {
     //   cacheTime: 5000      // length of time before inactive data gets removed from the cache.
     // }
@@ -21,15 +42,18 @@ export const RQSuperHeroesPage = () => {
     // }
   );
 
-  console.log({ isLoading, isFetching });
+  // console.log(isLoading, isFetching);
 
   if (isLoading) return <h2>Loading...</h2>;
   if (isError) return <h2>{error.message}</h2>;
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
-      {data?.data.map((hero, index) => {
+      {/* {data?.data.map((hero, index) => {
         return <div key={hero.id}>{hero.name}</div>;
+      })} */}
+      {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>;
       })}
     </>
   );
